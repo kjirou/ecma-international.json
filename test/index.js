4ebe614a00db978f78ba04559b0767acd638147b
kjirou <sorenariblog@gmail.com>
2016-02-19 15:59:32 +0900

    At first, tried to write by the manual

---
+++ test/index.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const tv4 = require('tv4');
+
+
+describe('ecma-international.json', () => {
+
+  it('should be', () => {
+    const ecmaInternatinalJsonData = require('../ecma-international.json');
+  });
+});
